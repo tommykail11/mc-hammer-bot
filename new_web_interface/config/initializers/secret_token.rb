7c9877bd6630d217a91f890c7155271191ce5d1a
Tommy Kail <tommykail@gmail.com>
2013-04-06 13:03:15 -0400

    Initial commit

---
+++ new_web_interface/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewWebInterface::Application.config.secret_token = '3a4c1e44ca74ae5fa94c3db39b03358fdd284e550dafc5d51b6bcafb07fa49f38ba7f9ed2dc02987e0c804ac73379cc3881e2de86827255171e0029ab83cfc91'
